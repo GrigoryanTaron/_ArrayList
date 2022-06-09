64aea84683183d838931b2583e7da2959dd83383
Taron Grigoryan <1991taron@gmail.com>
2022-06-09 09:35:49 +0400

    Add project files.

---
+++ _ArrayList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ArrayList
+{
+    public class _ArrayList
+    {
+        private Object[] _items;
+        private const int _defaultCapacity = 4;
+        private static  Object[] emptyArray = new Object [0];
+        public _ArrayList()
+        {
+            _items = emptyArray;
+        }
+        public _ArrayList(int capacity)
+        {
+            if(capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if(capacity==0)
+                _items=emptyArray;
+            else
+                _items=new Object[capacity];
+        }
+        public _ArrayList(ICollection c)
+        {
+            if(c == null)
+                throw new ArgumentNullException();
+            int count = c.Count;
+            if (count == 0)
+                _items = emptyArray;
+            else
+                _items=new Object[count];
+            AddRange(c);
+
+        }
+    }
+}
